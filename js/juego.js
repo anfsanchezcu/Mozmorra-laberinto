7eca16af157cdbb8ea69579ceff8642cf84b5944
Andres Sanchez <anfsanchezcu@unal.edu.co>
2021-08-17 10:09:19 -0500

    enemigos

--- js/juego.js
+++ js/juego.js
@@ -1,6 +1,12 @@
 var canvas;
-var fps=60;
+var fps=40;
 var ctx;
+var tileMap = new Image();
+
+var conejo;
+var enemigos = [];
+
+
 
 var anchoF = 25;
 var altoF=25;
@@ -9,20 +15,22 @@ var muro ='#222222';
 var camino='#3b3735';
 var llave='#bceb00';
 var puerta ="#2f346e"
+var win ="#9534eb";
 
 
 var escenario = [
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,1,1,0,0,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0],
-    [0,0,1,0,0,1,0,0,0,1,0,0,0,0,0,1,0,0,0,0],
-    [0,0,1,0,0,1,1,0,0,1,0,0,0,0,1,1,0,0,0,0],
-    [0,0,5,0,0,0,1,0,0,1,1,1,1,0,0,1,1,1,1,0],
-    [0,1,2,1,1,0,1,0,0,1,0,1,0,0,0,1,0,0,1,0],
-    [0,1,0,0,1,1,1,0,0,0,0,1,1,1,0,1,0,0,5,0],
-    [0,1,1,0,0,0,0,0,0,0,0,1,0,1,0,0,0,0,0,0],
-    [0,0,1,1,0,0,0,0,2,1,1,1,0,1,1,1,1,1,0,0],
+    [0,2,2,0,0,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0],
+    [0,0,2,0,0,2,0,0,0,2,0,0,0,0,0,2,0,0,0,0],
+    [0,0,2,0,0,2,2,0,0,2,0,0,0,0,2,2,0,0,0,0],
+    [0,0,2,0,0,0,2,0,0,2,2,2,2,0,0,2,2,2,2,0],
+    [0,2,2,2,2,0,2,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,0,0,2,2,2,0,0,0,0,2,2,2,0,2,0,0,3,0],
+    [0,2,2,0,0,0,0,0,0,0,0,2,0,2,0,0,0,0,0,0],
+    [0,0,2,2,0,0,0,0,2,2,2,2,0,2,2,2,2,1,0,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
-]
+];
+
 
 
 function borrarCanvas(){//al re establecer el tamaño de canvas se elimina toso el contenido
@@ -35,9 +43,7 @@ var corredor = function(x,y){
     this.llave =false;
 
     this.dibujar = function(){
-        ctx.fillStyle ='#ff2600';
-        ctx.fillRect(this.x*anchoF, this.y*altoF, anchoF,altoF);
-        
+        ctx.drawImage(tileMap,1*32,1*32,32,32,this.x*anchoF,this.y*altoF,anchoF,altoF);
     }
     this.margenes = function(x,y){
        
@@ -46,21 +52,38 @@ var corredor = function(x,y){
         return true;
     }
     this.getKey= function(){
-        console.log('x:'+this.x+' y:'+this.y+' escenario:'+escenario[y][x]);
-        if(escenario[this.y][this.x]==5){
-            escenario[this.y][this.x]=1;
+        //console.log('x:'+this.x+' y:'+this.y+' escenario:'+escenario[y][x]);
+        if(escenario[this.y][this.x]==3){
+            escenario[this.y][this.x]=2;
             this.llave = true;
         }
         this.win();
     }
+    this.fullWin=function(){
+        for(i=0;i<10;i++){
+            for(j=0;j<20;j++)
+                escenario[i][j]=10;
+        }
+    }
     this.win = function(){
-        if(escenario[this.y][this.x]==2 && this.llave ==true){
+        if(escenario[this.y][this.x]==1 && this.llave ==true){
             escenario[6][18]=5;
             this.llave=false;
             this.x=1;
             this.y=1;
-            console.log(escenario);
+            this.fullWin();
         }
+    }
+    this.colisionEnemigo=function(x,y){
+        console.log("conejo x: "+ this.x+ " y: "+this.y +"--- X: "+x+" Y:"+y)
+        if(this.x == x && this.y == y){
+         this.muere();
+        }
+    }
+    this.muere = function(){
+        this.x =1;
+        this.y =1;
+        escenario[6][18]=3;
     }
     this.up= function(){
         if(this.margenes(this.x,this.y-1)){
@@ -89,35 +112,88 @@ var corredor = function(x,y){
     
 }
 
+var enemigo = function(x,y){
+    this.x=x;
+    this.y=y;
+    this.delay = 10;
+    this.time=this.delay;
+    this.direccion = Math.floor(Math.random()*4);
+    this.dibujar=function(){
+        ctx.drawImage(tileMap,0,32,32,32,this.x*anchoF,this.y*altoF,anchoF,altoF);
+        //console.log("ya deberia esstae dibujado");
+    }
+    this.mover =function(){
+        
+        if(this.direccion == 0 && this.colision(this.x,this.y-1)==false){
+            this.y--;
+        }else if(this.direccion == 1 && this.colision(this.x,this.y+1)==false){
+            this.y++;
+        }else if(this.direccion == 2 && this.colision(this.x-1,this.y)==false){
+            this.x--;
+        }else if(this.direccion == 3 && this.colision(this.x+1,this.y)==false){
+            this.x++;
+        }
+        this.direccion = Math.floor(Math.random()*4);
+        
+    }
+    this.colision = function(x,y){
+        if (escenario[y][x]==0)
+            return true;
+        return false;
+    }
+    this.activar = function(){
+        conejo.colisionEnemigo(this.x , this.y);
+        if(this.time<=0){
+            this.time =this.delay;
+            this.mover();
+        }else{
+            this.time--;
+        }
+    }
+}
+
 function dibujarEscenario(){
     var color;
-    for(var i=0;i<20;i++){
-        for(var j=0;j<10;j++){
-            if(escenario[j][i]==0){
+    for(var i=0;i<10;i++){
+        for(var j=0;j<20;j++){
+            /* if(escenario[i][j]==0){
                 color=muro;
-            }else if(escenario[j][i]==1){
+            }else if(escenario[i][j]==1){
                 color = camino;
-            }else if(escenario[j][i]==5){
+            }else if(escenario[i][j]==5){
                 color=llave;
-            }else if(escenario[j][i]==2){
+            }else if(escenario[i][j]==2){
                 color = puerta;
+            }else if(escenario[i][j]==10){
+                color=win;
             }
             ctx.fillStyle = color;
-            ctx.fillRect(i*anchoF, j*altoF,anchoF, altoF);
+            ctx.fillRect(i*anchoF, j*altoF,anchoF, altoF); */
+            var cuadro=escenario[i][j];
+            ctx.drawImage(tileMap,cuadro*32,0,32,32,j*anchoF,altoF*i,anchoF,altoF);// parmetro: imagen, en x desde donde empieza, en y desde donde empieza,dimencion x,dimencion y,donde poner x,donde poner y
         }
 
     }
 }
 
-var conejo = new corredor(1,1);
 
 
 
 function inicializacion(){
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext('2d');
-    setInterval(()=>main(), 1000/fps);
+    tileMap.src='img/tilemap.png';
 
+    conejo = new corredor(1,1);
+    enemigos =[];
+
+    enemigos.push(new enemigo(1,3));
+    enemigos.push(new enemigo(15,2));
+    enemigos.push(new enemigo(10,9));
+
+
+
+    setInterval(()=>main(), 1000/fps);
     document.addEventListener('keydown',function(e){
         //console.log(e.keyCode);
         if(e.keyCode==37){conejo.left(); }
@@ -129,9 +205,16 @@ function inicializacion(){
     )
 }
 function main(){
+    
    borrarCanvas();
    dibujarEscenario();
    conejo.dibujar();
+  
+   for(var i =0 ; i<3;i++){
+       enemigos[i].dibujar();
+       enemigos[i].activar();
+   }
+   
    
 
 }
